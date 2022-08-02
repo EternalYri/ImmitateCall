22deafdf8485ce586963547198d7c2207c08448c
EternalYri <97452847+EternalYri@users.noreply.github.com>
2022-08-02 16:21:34 +0300

    main

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 
 export class AuthGuard implements CanActivate{
-  constructor( private auth: AuthService) {}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : boolean {
-    // return this.auth.flag;
-    return true;
+  constructor( private auth: AuthService, private redirect: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.auth.flag) {
+      return true;
+    } else {this.redirect.navigate(["/"])
+  return false};
   }
 }
